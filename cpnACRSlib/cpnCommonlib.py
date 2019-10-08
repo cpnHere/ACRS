bc37b8dc54ec7182b026d473651126ac04bbd260
cpnHere <cpn.here@umbc.edu>
2019-10-08 18:43:34 -0400

    write_latex_table moved and add more comments

--- cpnACRSlib/cpnCommonlib.py
+++ cpnACRSlib/cpnCommonlib.py
@@ -105,6 +105,28 @@ def idx_closest(arr,val):
     Returns the clossest index of arr to value val.
     '''
     return abs(arr-val).argmin()
+def write_latex_table(X,table_name,colnames=None,rownames=None,stl='%0.2f'):
+    '''
+    To write arrays as latex tables
+    colnames should have ALL column names, including the LABEL for ROWs.
+    '''
+    if colnames is None:
+        colnames=['col%d'%(i) for i in range(X.shape[1])]
+    if rownames is None:
+        rownames=['row%d'%(i) for i in range(X.shape[0])]
+    colnames=[r.replace('_','\_') for r in colnames]
+    rownames=[r.replace('_','\_') for r in rownames]
+    cols=colnames[:-1]
+    f1=open(table_name,'w')
+    [f1.write(i+' & ') for i in cols]
+    f1.write(colnames[-1]+' \\\\'+'\n\hline\n')
+    for i in range(np.size(rownames)):
+        f1.write(rownames[i])
+        for j in range(np.size(colnames[1:])):
+            f1.write('&'+stl%(X[i,j-1]))
+        f1.write(' \\\\'+'\n')
+    f1.write('\hline\n')
+    f1.close()
 '''
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 FIGURES
@@ -323,24 +345,3 @@ def multiple_ticks(ax_XY,major_step,minor_step=None,major_format='%0.1f'):
     else:
         ax_XY.set_minor_locator(AutoMinorLocator())
 
-def write_latex_table(X,table_name,colnames=None,rownames=None,stl='%0.2f'):
-    '''
-    To write arrays as latex tables
-    '''
-    if colnames is None:
-        colnames=['col%d'%(i) for i in range(X.shape[1])]
-    if rownames is None:
-        rownames=['row%d'%(i) for i in range(X.shape[0])]
-    colnames=[r.replace('_','\_') for r in colnames]
-    rownames=[r.replace('_','\_') for r in rownames]
-    cols=colnames[:-1]
-    f1=open(table_name,'w')
-    [f1.write(i+' & ') for i in cols]
-    f1.write(colnames[-1]+' \\\\'+'\n\hline\n')
-    for i in range(np.size(rownames)):
-        f1.write(rownames[i])
-        for j in range(np.size(colnames[1:])):
-            f1.write('&'+stl%(X[i,j-1]))
-        f1.write(' \\\\'+'\n')
-    f1.write('\hline\n')
-    f1.close()
