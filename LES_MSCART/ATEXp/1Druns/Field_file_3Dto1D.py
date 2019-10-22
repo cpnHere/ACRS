ca331ae35215e708ab338af1f0fb7c77261a5dbe
cpnHere <cpn.here@umbc.edu>
2019-10-22 07:19:17 -0400

    First time added to run new cases with hight detector altitudes.

---
+++ LES_MSCART/ATEXp/1Druns/Field_file_3Dto1D.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+********************************************
+Created on Wed Apr 26 21:32:36 2017
+by
+Chamara Rajapakshe
+(cpn.here@umbc.edu)
+********************************************
+To convert 3D field files to individual column field files to run MSCART separately.
+09/01/2017:
+    To handle fractal cloud case. Trying to make it compatible with any LES_field object
+05/14/2018:
+    For the LES cases. Uses the "actual 1D feature" of MSCART
+"""
+import numpy as np
+import cpnLES_MSCARTlib as lib
+
+
+band='2p13'
+d3D=lib.LES_field('ATEXp_dharma_013067_b2p13.nc',dpath='../')
+d3D.readLES_field()
+#i1D=lib.LES_field('OP_A5.nc')
+#i1D.readLES_field()
+
+def dobins():
+    for i in np.arange(0,d3D.ny):
+        for j in np.arange(0,d3D.nx):
+            fobj=lib.LES_field(d3D.fname.split('.',1)[0]+'y'+str(i)+'x'+str(j)+'.nc')
+            fobj.nxp=2
+            fobj.nyp=2
+            fobj.nzp=d3D.nzp
+            fobj.nx=1
+            fobj.ny=1
+            fobj.nz=d3D.nz
+            fobj.nz3=1#d3D.nz3 #Number of horizontally inhomogeneous layers
+            fobj.iz3l=1
+            fobj.npar1=d3D.npar3
+            fobj.npar3=2#d3D.npar3
+            fobj.nkd=d3D.nkd
+            fobj.nstokes=d3D.nstokes
+            fobj.nang=d3D.nang
+            fobj.npf=d3D.npf
+            fobj.npsfc=d3D.npsfc
+            fobj.xgrd=np.array([0,1])
+            fobj.ygrd=np.array([0,1])
+        
+            fobj.zgrd=d3D.zgrd
+            fobj.extp1d=d3D.extp3d[:,:,i,j]
+            fobj.omgp1d=d3D.omgp3d[:,:,i,j]
+            fobj.jpfp1d=d3D.jpfp3d[:,:,i,j]
+            fobj.rpfp1d=d3D.rpfp3d[:,:,i,j]
+            fobj.extp3d=np.zeros((fobj.npar3,fobj.nz3,1,1),dtype=float)
+            fobj.omgp3d=np.ones ((fobj.npar3,fobj.nz3,1,1),dtype=float)
+            fobj.jpfp3d=np.zeros((fobj.npar3,fobj.nz3,1,1),dtype=float)
+            fobj.rpfp3d=np.zeros((fobj.npar3,fobj.nz3,1,1),dtype=float)
+            fobj.absg1d=d3D.absg1d
+            fobj.wkd=d3D.wkd
+            fobj.scatAng=d3D.scatAng
+            fobj.PMatVal=d3D.PMatVal
+            fobj.tsfc2d=d3D.tsfc2d[i,j]
+            fobj.jsfc2d=d3D.jsfc2d[i,j]
+            fobj.psfc2d=d3D.psfc2d[i,j,:]
+
+            fobj.writeLES_field(fobj.fname,dpath='LESb'+band+'_bins/')
+	
+
+dobins()
---
+++ LES_MSCART/ATEXp/1Druns/LES_MSCART_column_run.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+********************************************
+Created on Fri Apr 28 17:22:48 2017
+by
+Chamara Rajapakshe
+(cpn.here@umbc.edu)
+********************************************
+LES_MSCART column by columns runs moderator
+Dependancies:
+    LES_MSCART_setup.py
+05/01/2017:
+    set the job id of the slurm from 0 to 16383(16384-1)
+    This script will create *.nml files for each column and run MSCART.
+    Setup the dtector specs in LES_MSCART_setup.py
+05/08/2017:
+    Adde _NPH in lin 41 to handle some readings in the LES_MSCARTlib
+05/15/2018:
+    To run LES MSCART 1D
+"""
+import os,fnmatch,sys
+
+s_the=float(sys.argv[1])
+vphi=float(sys.argv[2])
+field_path=str(sys.argv[3])
+field_pat=str(sys.argv[4])
+job=int(sys.argv[5])
+MSCART=str(sys.argv[6])
+NPH=str(sys.argv[7])
+n_grid=int(sys.argv[8])
+over_write=True
+
+print('Assume n_xgrd=n_ygrd=%d'%n_grid)
+i=str(job/n_grid)
+j=str(job%n_grid)
+fn_pattern=field_pat+'y'+i+'x'+j+'.nc'
+for f in os.listdir(field_path):
+    if fnmatch.fnmatch(f,fn_pattern):
+        os.system("echo "+f)
+#        cmnd_ln="fn=$(python LES_MSCART_setup.py "+str(vphi)+" "+field_path+f+\
+#        " 2>&1)\n echo ${fn}\n "+MSCART+" 10 "+NPH+" 0 "+\
+#        "${fn}.nml results/${fn}_"+NPH+".nc"
+#        print(cmnd_ln)
+        run_setup="fn=$(python LES_MSCART_column_setup.py "+str(s_the)+" "+\
+                        str(vphi)+" "+field_path+f+" 2>&1)\n echo ${fn}\n "
+        run_MSCART=MSCART+" 10 "+NPH+" 0 ${fn}.nml results/${fn}_NPH"+NPH+".nc"
+        check_n_run="test=$(python checking_previous_runs.py results/${fn}_NPH"+NPH+".nc 2>&1)\n"+\
+        "echo checking_previous_runs.py gave ${test}\n"+\
+        "if [ $test -eq 1 ]; then\n"+\
+        "\t echo Output file already exist\n"+\
+        "else\n"+\
+        "\t  "+run_MSCART+"\nfi"
+        if over_write:
+            print('Going to overite the existing files....')
+            os.system(run_setup+run_MSCART)
+        else:
+            #os.system('${fn}')
+            print('Only execute if a result .nc file does not exist.')
+            os.system(run_setup+check_n_run)
+#        os.path.exists
+#        sys.exit(out_file)
+#echo ${fn}
+#${MSCART} 10 ${NPH} 0 ${fn}.nml results/${fn}_NPH${NPH}.nc
