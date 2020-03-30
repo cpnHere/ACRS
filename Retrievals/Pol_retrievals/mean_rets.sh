698add15b3123be14e7c5462d8b833732d3fa5a9
cpnHere <cpn.here@umbc.edu>
2020-03-30 13:12:59 -0400

    For RICO

--- Retrievals/Pol_retrievals/mean_rets.sh
+++ Retrievals/Pol_retrievals/mean_rets.sh
@@ -4,8 +4,8 @@
 #
 for sza in 120 140 160
 do
-	for res in nt 0p1km 0p5km 1p0km 5p0km
+	for res in nt 0p5km 1p0km 2p0km 5p0km
 	do
-		python pol_cloud_ret.py 0p860 $sza 3D Breon mean _pol_ret_V7 DY $res
+		python pol_cloud_ret.py 0p860 $sza 3D Breon mean _pol_ret_V7 RC $res
 	done
 done
