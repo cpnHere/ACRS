dfc20119478ed082e47f382d52df314a9af07960
cpnHere <cpn.here@umbc.edu>
2020-07-11 10:52:25 -0400

    I/O path handled

--- LUTs/using_DISORT/disort_driver.sh
+++ LUTs/using_DISORT/disort_driver.sh
@@ -2,8 +2,10 @@
 # Chamara Rajapakshe (2020)
 #----------------------------------------------
 jobid=$1
-cp ${jobid}_inputFile.dat inputFile.dat
+inp=$2
+outp=$3
+cp ${inp}${jobid}_inputFile.dat inputFile.dat
 rm INTENSITY.dat
 time ./single_lay_cld.exe > single_lay_cld.out
-cp -i INTENSITY.dat results/${jobid}_INTENSITY.dat
-cp -i single_lay_cld.out results/${jobid}_single_lay_cld.out 
+cp -i INTENSITY.dat ${outp}${jobid}_INTENSITY.dat
+cp -i single_lay_cld.out ${outp}${jobid}_single_lay_cld.out 
