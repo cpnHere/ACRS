518434faaa471b1099b39322fdec2bbc27a30c2a
cpnHere <cpn.here@umbc.edu>
2020-08-21 16:18:33 -0400

    Albedo I/O bug fixed

--- LUTs/using_DISORT/READ_VAL.f90
+++ LUTs/using_DISORT/READ_VAL.f90
@@ -1,11 +1,12 @@
-      SUBROUTINE READ_VAL( FNAME,COT,MU0,PMOM)
+      SUBROUTINE READ_VAL( FNAME,COT,SCA,MU0,PMOM)
 ! c To read input values from a text file
       CHARACTER*(*)      FNAME
-      REAL            COT, MU0
+      REAL            COT, MU0, SCA
       INTEGER K
       REAL, DIMENSION (500) :: PMOM
       OPEN ( 10, FILE=FNAME )
       READ ( 10, *) COT
+      READ ( 10, *) SCA 
       READ ( 10, *) MU0
       DO  60  K = 1, 500
          READ (10,*) PMOM(K)
